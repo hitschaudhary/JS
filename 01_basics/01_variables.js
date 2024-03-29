086e0803001fdce91bc8fd7ceb0d1671db75ecc5
hitschaudhary <124068463+hitschaudhary@users.noreply.github.com>
2024-03-29 16:22:52 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 142345
+let accountEmail = "hiteshchaudhary@google.com"
+var accountPassword = "123456"
+accountcity = "Gandhinagar"
+let accountname;
+
+//  accountId = 3 //not allowed
+ accountEmail = "hits@google.com"
+ accountPassword = "54321"
+ accountcity = "ahemdabad"
+
+
+ console.log("accountId");
+
+ console.table([accountId, accountEmail, accountPassword, accountcity, accountname])
