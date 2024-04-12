77914d1cb5dcb9c2fecff80f6cac804495b814c3
hitschaudhary <124068463+hitschaudhary@users.noreply.github.com>
2024-04-12 07:08:33 +0000

    object and jeson

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,7 +27,7 @@ const obj4 = {5: "a", 6: "b"}
 //const obj3 = Object.assign({}, obj1, obj2, obj4)
 
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+//console.log(obj3);
 
 const users = [
     {
@@ -40,11 +40,39 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(Object.keys(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isloggedIn'));
+// console.log(tinderUser.hasOwnProperty('isloggedIn'));
+
+
+
+/*                */
+
+
+const course = {
+     coursename: "js hindi",
+     price: "999",
+     courseinstructor: "hitesh"
+}
+
+const {courseinstructor: instructor} = course
+
+//console.log(courseinstructor);
+console.log(instructor);
+
+// {
+//     name: "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
