77cc39cc159e34f9cef83cbf459d13623b7eb14e
hitschaudhary <124068463+hitschaudhary@users.noreply.github.com>
2024-04-12 09:20:11 +0000

    basics of function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,5 +37,43 @@ function sayMyName() {
         return `${username} just logged in`
     }
     
-    console.log(loginUserMessage("hitesh"));
+    //console.log(loginUserMessage("hitesh"));
     
+    /*          */
+
+    function calculateCartPrice(val1, val2, ...num1) { // ... => rest opratore
+        return num1
+    }
+
+    //console.log(calculateCartPrice(200, 300, 400, 2000, 5000));
+
+    const user = {
+        username: "hitesh",
+        price: 199
+    }
+
+    function handleObject(anyobject){
+        console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    }
+
+    //handleObject(user)
+    handleObject({
+        username: "sam",
+        price: 399
+    })
+    
+
+    const myNewArray = [100, 200, 300, 500, 900]
+    
+    function returnSecondValue(getArray){
+        return getArray[1]
+    }
+
+   // console.log(returnSecondValue(myNewArray));
+   console.log(returnSecondValue([100, 200, 300, 500, 900]));
+
+
+
+
+
+
