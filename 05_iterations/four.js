0347d130d29fe88ec61d23c8506d08fadf542000
hiteshChaudhary <124068463+hitschaudhary@users.noreply.github.com>
2024-05-22 03:26:02 +0000

    For_in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,26 @@
+const myObject = { //object
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+//    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"] //array
+
+for (const key in programming) {
+//      console.log(programming[key]);
+}
+
+const map = new Map() // map is not iterateble
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) { 
+    console.log(key);
+}
